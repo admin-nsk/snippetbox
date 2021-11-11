71bb1c59cc2481cd1119c68b29b4e25b65e6caf0
Alexey Petrov <adm_sib@mail.ru>
2021-11-11 15:31:09 +0700

    Добавлен обработчик запросов в ЬБД

--- pkg/pkg/mod/github.com/admin-nsk/snippetbox@v0.0.0-20211105013355-2257783e30d1/cmd/web/handlers.go
+++ pkg/pkg/mod/github.com/admin-nsk/snippetbox@v0.0.0-20211105013355-2257783e30d1/cmd/web/handlers.go
@@ -47,17 +47,6 @@ func (app *application) createSnippet(writer http.ResponseWriter, request *http.
 		app.clientError(writer, http.StatusMethodNotAllowed)
 		return
 	}
-
-	title := "Golang очень быстрый!"
-	content := "Golang хороший язык"
-	expires := "7"
-
-	id, err := app.snippets.Insert(title, content, expires)
-	if err != nil {
-		app.ServerError(writer, err)
-		return
-	}
-
-	http.Redirect(writer, request, fmt.Sprintf("/snippet?id=%d", id), http.StatusSeeOther)
+	writer.Write([]byte("Создание заметки"))
 }
 
--- pkg/pkg/mod/github.com/admin-nsk/snippetbox@v0.0.0-20211105013355-2257783e30d1/cmd/web/main.go
+++ pkg/pkg/mod/github.com/admin-nsk/snippetbox@v0.0.0-20211105013355-2257783e30d1/cmd/web/main.go
@@ -1,43 +1,29 @@
 package main
 
 import (
-	"database/sql"
 	"flag"
 	"log"
 	"net/http"
 	"os"
 	"path/filepath"
-
-	"snippetbox/pkg/models/mysql"
-
-	_ "github.com/go-sql-driver/mysql"
 )
 
 type application struct {
 	infoLog *log.Logger
 	errorLog *log.Logger
-	snippets *mysql.SnippetModel
 }
 
 func main() {
 	addr := flag.String("addr", ":4000", "Сетевой адресс HTTP")
-	dsn := flag.String("dsn", "web:webadmin@/snippetbox?parseTime=true", "Название MySQL источника данных")
+
 	flag.Parse()
 
 	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
 	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
 
-	db, err := openDB(*dsn)
-	if err != nil {
-		errorLog.Fatal(err)
-	}
-
-	defer db.Close()
-
 	app := &application{
 		errorLog: errorLog,
 		infoLog: infoLog,
-		snippets: &mysql.SnippetModel{DB: db},
 	}
 
 	srv := &http.Server{
@@ -47,19 +33,8 @@ func main() {
 	}
 
 	infoLog.Printf("Запуск сервера на http: %s\n", *addr)
-	err = srv.ListenAndServe()
+	err := srv.ListenAndServe()
 	errorLog.Fatal(err)
-}
-
-func openDB(dsn string) (*sql.DB, error){
-	db, err := sql.Open("mysql", dsn)
-	if err != nil {
-		return nil, err
-	}
-	if err = db.Ping(); err != nil {
-		return nil, err
-	}
-	return db, nil
 }
 
 type neuteredFileSystem struct {
